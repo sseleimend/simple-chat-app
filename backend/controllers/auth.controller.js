cc7fa18989c82e797b6b1f3205e27c2492e7b569
Victor Seleimend <sseleimend@gmail.com>
2025-07-30 10:05:22 -0300

    Add auth.controller.js

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req, res) => {
+  console.log("signup route");
+};
+
+export const login = (req, res) => {
+  console.log("login route");
+};
+
+export const logout = (req, res) => {
+  console.log("logout route");
+};
