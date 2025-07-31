a38786a9b72a8b22aec8e019ec61da74d158bcd8
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 13:49:51 -0300

    Add login page

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router";
+
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
+      <div className="w-full p-6 rounded-lg shadow-md backdrop-filter backdrop-blur-lg">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Login <span className="text-blue-500">ChatApp</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2" htmlFor="">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter username"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2" htmlFor="">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <Link
+            to={"/signup"}
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block"
+          >
+            Don&apos;t have an account?
+          </Link>
+
+          <div className="btn btn-block btn-sm mt-2">Login</div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
