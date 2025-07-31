d3cad67693a75f3c8790193aa0e82bfe01ed5765
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 11:47:49 -0300

    Update server.js by adding user routes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectToMongo from "./db/connectToMongo.js";
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongo();
