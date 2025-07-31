9da63c0b494cdbc44d1e171b9ab57348fe263cfa
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 11:30:55 -0300

    Update server.js by adding message routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectToMongo from "./db/connectToMongo.js";
 
 dotenv.config();
@@ -11,8 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   connectToMongo();
