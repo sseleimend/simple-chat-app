6191f7610517675e33c7ccb44d69dcf4a6106d26
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 13:53:35 -0300

    Add sign up page

---
+++ frontend/src/pages/signup/SignUp.jsx
@@ -0,0 +1,67 @@
+import { Link } from "react-router";
+import GenderCheckbox from "./GenderCheckbox.jsx";
+
+const SignUp = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-w-96 mx-auto ">
+      <div className="w-full p-6 rounded-lg shadow-md backdrop-filter backdrop-blur-lg">
+        <h1 className="text-3xl font-semibold text-center text-gray-300">
+          Sign Up <span className="text-blue-500">ChatApp</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2" htmlFor="">
+              <span className="text-base label-text">Full Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="John Doe"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2" htmlFor="">
+              <span className="text-base label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder="johndoe"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2" htmlFor="">
+              <span className="text-base label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <div>
+            <label className="label p-2" htmlFor="">
+              <span className="text-base label-text">Confirm Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm password"
+              className="w-full input input-bordered h-10"
+            />
+          </div>
+          <GenderCheckbox />
+          <Link
+            to={"/login"}
+            className="text-sm hover:underline hover:text-blue-600 mt-4 inline-block"
+          >
+            Already have an account?
+          </Link>
+
+          <div className="btn btn-block btn-sm mt-2">Sign Up</div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
