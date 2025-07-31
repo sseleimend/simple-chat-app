2f2cc0ff5325dae9a1ef775c4d3d74cb26755c58
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 20:05:11 -0300

    Update home component by adding sidebar and message container component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,13 @@
+import { MessageContainer } from "../../components/messages/MessageContainer.jsx";
+import { Sidebar } from "../../components/sidebar/Sidebar.jsx";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex sm:h-[450px] md:h-[550px]  rounded-lg overflow-hidden backdrop-filter backdrop-blur-lg">
+      <Sidebar />
+      <MessageContainer />
+    </div>
+  );
 };
 
 export default Home;
