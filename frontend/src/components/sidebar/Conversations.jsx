822424096a79b0810dc03fe26eab954c3342e527
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 20:04:21 -0300

    Add conversation container component

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,14 @@
+import { Conversation } from "./Conversation.jsx";
+
+export const Conversations = () => {
+  return (
+    <div className="flex flex-col py-2 overflow-auto">
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </div>
+  );
+};
