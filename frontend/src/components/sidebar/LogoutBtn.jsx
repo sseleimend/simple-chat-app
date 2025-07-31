fb5e1503a6e13c3a990cb1556a2f6ac7c8d7813b
Victor Seleimend <sseleimend@gmail.com>
2025-07-31 20:04:36 -0300

    Add logout btn component

---
+++ frontend/src/components/sidebar/LogoutBtn.jsx
@@ -0,0 +1,9 @@
+import { BiLogOut } from "react-icons/bi";
+
+export const LogoutBtn = () => {
+  return (
+    <div className="mt-auto">
+      <BiLogOut className="size-6 text-white cursor-pointer" />
+    </div>
+  );
+};
