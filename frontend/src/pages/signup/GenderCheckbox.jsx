431435ae6680fa16c2d2f6e4e64a0ff9ea76a1ed
Victor Seleimend <sseleimend@gmail.com>
2025-08-01 11:14:34 -0300

    Update gender checkbox by implementing state management

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,23 +1,35 @@
-const GenderCheckbox = () => {
+const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
   return (
     <div className="flex mt-2 gap-2">
       <div className="form-control">
-        <label className="label gap-2 cursor-pointer">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Male</span>
           <input
             type="radio"
             name="gender"
             className="radio border-slate-900"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
           />
         </label>
       </div>
       <div className="form-control">
-        <label className="label gap-2 cursor-pointer">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text">Female</span>
           <input
             type="radio"
             name="gender"
             className="radio border-slate-900"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
           />
         </label>
       </div>
