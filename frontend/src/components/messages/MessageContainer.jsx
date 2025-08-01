60ff88ca01a7a395ae16c5fe51720383e060d402
Victor Seleimend <sseleimend@gmail.com>
2025-08-01 13:07:23 -0300

    Update message container component by replacing noChatSelected with selectedConversation from the store

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,27 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation.js";
 import { MessageInput } from "./MessageInput.jsx";
 import { Messages } from "./Messages.jsx";
 
 import { TiMessages } from "react-icons/ti";
 
 export const MessageContainer = () => {
-  const noChatSelected = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* <Header /> */}
           <div className="bg-slate-500 py-2 px-4 mb-2">
             <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John Doe</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
           </div>
 
           <Messages />
