69d206e7a5c889332c76cbcca36b1bcfaf22032e
Victor Seleimend <sseleimend@gmail.com>
2025-08-01 11:12:13 -0300

    Update App.jsx by implementing route protection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,29 @@
-import { Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router";
 import Login from "./pages/login/Login.jsx";
 import SignUp from "./pages/signup/Signup.jsx";
 import Home from "./pages/home/Home.jsx";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext.jsx";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/"
+          element={!authUser ? <Navigate to="/login" /> : <Home />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
       </Routes>
+      <Toaster />
     </div>
   );
 }
