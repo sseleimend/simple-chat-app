c10d90627648b2e0cce7687a36ea6da9eeac4856
Victor Seleimend <sseleimend@gmail.com>
2025-08-01 13:09:15 -0300

    Update conversations component by adding dynamically the conversation component

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,14 +1,21 @@
+import { useGetConversations } from "../../hooks/useGetConversations.js";
+import { getRandomEmoji } from "../../utils/emojis.js";
 import { Conversation } from "./Conversation.jsx";
 
 export const Conversations = () => {
+  const { conversations, loading } = useGetConversations();
+
   return (
     <div className="flex flex-col py-2 overflow-auto">
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
-      <Conversation />
+      {conversations.map((c, idx) => (
+        <Conversation
+          key={c._id}
+          conversation={c}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversations.length - 1}
+        />
+      ))}
+      {loading ? <span className="loading loading-spinner"></span> : null}
     </div>
   );
 };
