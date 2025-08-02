5bd702d0b2870b09db3051eb2e36470c9cd465c2
Victor Seleimend <sseleimend@gmail.com>
2025-08-02 16:35:38 -0300

    Update message component with dynamic data

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,19 +1,29 @@
-export const Message = () => {
+import { useAuthContext } from "../../context/AuthContext.jsx";
+import { extractTime } from "../../utils/extractTime.js";
+import useConversation from "../../zustand/useConversation.js";
+
+export const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat chat-end" : "chat chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
   return (
-    <div className="chat chat-end">
+    <div className={chatClassName}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
-            alt="user avatar"
-          />
+          <img src={profilePic} alt="user avatar" />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>
-        Hi! What&apos;s Up?
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
       </div>
       <div className={`chat-footer opacity-50 text-xs flex gap-1 items-center`}>
-        12:42
+        {extractTime(message.createdAt)}
       </div>
     </div>
   );
