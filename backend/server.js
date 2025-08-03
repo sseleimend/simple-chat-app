75ae7cbe63a1d519214a89933166c7124e709adb
Victor Seleimend <sseleimend@gmail.com>
2025-08-03 12:49:20 -0300

    Update server.js by implementing production server config

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -10,6 +11,8 @@ import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -18,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get((req, res) =>
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+);
 
 server.listen(PORT, () => {
   connectToMongo();
