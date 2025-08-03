49a90ac7f54c2dbb29f3fdf3078cc827116d1a42
Victor Seleimend <sseleimend@gmail.com>
2025-08-03 11:56:46 -0300

    Add socket.js

---
+++ backend/socket/socket.js
@@ -0,0 +1,31 @@
+import { Server } from "socket.io";
+import http from "http";
+import e from "express";
+
+const app = e();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5000"],
+    methods: ["GET", "POST"],
+  },
+});
+
+const usersConnected = new Map();
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  if (typeof userId !== "undefined") {
+    usersConnected.set(userId, socket.id);
+    io.emit("getOnlineUsers", [...usersConnected.keys()]);
+  }
+  socket.on("disconnect", () => {
+    usersConnected.delete(userId);
+
+    io.emit("getOnlineUsers", [...usersConnected.keys()]);
+  });
+});
+
+export { app, io, server };
